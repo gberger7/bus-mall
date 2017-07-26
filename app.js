43ec9cce1fa03a1be57afc7dcbc07712f656e277
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-26 14:49:44 -0700

    Duplicate Pics Fixed

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 'use strict';
 
+Pics.all = [];
+
 Pics.allNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'wine-glass.jpg', 'water-can.jpg'];
 
 function Pics(name) {
   this.name = name;
-  this.source = 'bus-images/' + this.name + '.jpg';
+  this.source = 'bus-images/' + this.name;
   this.amountShown = 0;
   this.timesClicked = 0;
   Pics.all.push(this);
 }
 // debugger;
-
-Pics.all = [];
-new Pics('bag');
-console.log(Pics.all, 'all pics');
-
-// var previouslyShown = [];
-for (var i = 0; i < Pics.all.length; i++);
-
-// var Pics(Pics.allNames[i]);
+for (var i = 0; i < Pics.allNames.length; i++) {
+  new Pics(Pics.allNames[i]);
+}
 
 Pics.leftImg = document.getElementById('left');
 Pics.centerImg = document.getElementById('center');
@@ -35,28 +31,30 @@ function displayImages() {
   var numbers = [];
   numbers[0] = randomNum();
   numbers[1] = randomNum();
-  Pics.leftImg.src = 'bus-images/bag.jpg';
-  Pics.centerImg.src = 'bus-images/dog-duck.jpg';
-  Pics.rightImg.src = 'bus-images/unicorn.jpg';
 
-  // while (numbers[0] === numbers[1]) {}
-  // numbers[1] = randomNum();
-  // numbers[2] = randomNum();
-  // while (numbers[2] === numbers[1] || numbers[2] ===
-  //   numbers[0]) {
-  //   numbers[2] = randomNum();
-  // }
-  // Pics.leftImg.src = Pics.all[numbers[0]].source;
-  // centerImg.src = Pics.all[numbers[1]].source;
-  // rightImg.src = Pics.all[numbers[2]].source;
-  // leftImg.alt = Pics.all[numbers[0]].name;
-  // centerImg.alt = Pics.all[numbers[1]].name;
-  // rightImg.alt = Pics.all[numbers[2]].name;
-  // Pics.all[numbers[0]].amountShown += 1;
-  // Pics.all[numbers[1]].amountShown += 1;
-  // Pics.all[numbers[2]].amountShown += 1;
-  // console.log(numbers, 'currently showing');
-  // previouslyShown = numbers;
+  while (numbers[0] === numbers[1]) {
+    console.log('Dupe prevented');
+  numbers[1] = randomNum();
+}
+  numbers[2] = randomNum();
+  while (numbers[2] === numbers[1] || numbers[2] ===
+    numbers[0]) {
+    numbers[2] = randomNum();
+  }
+  Pics.leftImg.src = Pics.all[randomNum()].source;
+  Pics.centerImg.src = Pics.all[randomNum()].source;
+  Pics.rightImg.src = Pics.all[randomNum()].source;
+  Pics.leftImg.src = Pics.all[numbers[0]].source;
+  Pics.centerImg.src = Pics.all[numbers[1]].source;
+  Pics.rightImg.src = Pics.all[numbers[2]].source;
+  Pics.leftImg.alt = Pics.all[numbers[0]].name;
+  Pics.centerImg.alt = Pics.all[numbers[1]].name;
+  Pics.rightImg.alt = Pics.all[numbers[2]].name;
+  Pics.all[numbers[0]].amountShown += 1;
+  Pics.all[numbers[1]].amountShown += 1;
+  Pics.all[numbers[2]].amountShown += 1;
+  console.log(numbers, 'currently showing');
+
 }
 
 function showList() {
