02a2c4a473a21fcb4d563bb866dfb7841178b048
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-25 17:14:45 -0700

    Working on getting images implemented

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+Pics.allNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'wine-glass.jpg', 'water-can.jpg'];
 
 function Pics(name) {
   this.name = name;
@@ -8,15 +9,14 @@ function Pics(name) {
   this.timesClicked = 0;
   Pics.all.push(this);
 }
-
+// debugger;
 
 Pics.all = [];
-console.log(Pics.all, 'all pics');
 new Pics('bag');
-// Pics.allNames = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine','ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+console.log(Pics.all, 'all pics');
 
-var previouslyShown = [];
-for(var i = 0; i < Pics.all.length; i++);
+// var previouslyShown = [];
+for (var i = 0; i < Pics.all.length; i++);
 
 // var Pics(Pics.allNames[i]);
 
@@ -25,65 +25,68 @@ Pics.centerImg = document.getElementById('center');
 Pics.rightImg = document.getElementById('right');
 Pics.container = document.getElementById('imageContainer');
 
-function randomNum(){
-  return Math.floor(Math .random() * Pics.all.length);
+function randomNum() {
+  return Math.floor(Math.random() * Pics.all.length);
 }
 
-function displayImages(){
+function displayImages() {
 
   // console.log(previouslyShown, 'peviously shown images');
   var numbers = [];
   numbers[0] = randomNum();
   numbers[1] = randomNum();
-  while(numbers[0] === numbers[1]) {
-  }
-  numbers[1] = randomNum();
-  numbers[2] = randomNum();
-  while(numbers[2] === numbers[1] || numbers[2] ===
-    numbers[0]) {
-      numbers[2] = randomNum();
-    }
-    leftImg.src = Pics.all[numbers[0]].source;
-    centerImg.src = Pics.all[numbers[1]].source;
-    rightImg.src = Pics.all[numbers[2]].source;
-    leftImg.alt = Pics.all[numbers[0]].name;
-    centerImg.alt = Pics.all[numbers[1]].name;
-    rightImg.alt = Pics.all[numbers[2]].name;
-    Pics.all[numbers[0]].amountShown += 1;
-    Pics.all[numbers[1]].amountShown += 1;
-    Pics.all[numbers[2]].amountShown += 1;
-    console.log(numbers, 'currently showing');
-    previouslyShown = numbers;
-  }
+  Pics.leftImg.src = 'bus-images/bag.jpg';
+  Pics.centerImg.src = 'bus-images/dog-duck.jpg';
+  Pics.rightImg.src = 'bus-images/unicorn.jpg';
 
-function showList () {
+  // while (numbers[0] === numbers[1]) {}
+  // numbers[1] = randomNum();
+  // numbers[2] = randomNum();
+  // while (numbers[2] === numbers[1] || numbers[2] ===
+  //   numbers[0]) {
+  //   numbers[2] = randomNum();
+  // }
+  // Pics.leftImg.src = Pics.all[numbers[0]].source;
+  // centerImg.src = Pics.all[numbers[1]].source;
+  // rightImg.src = Pics.all[numbers[2]].source;
+  // leftImg.alt = Pics.all[numbers[0]].name;
+  // centerImg.alt = Pics.all[numbers[1]].name;
+  // rightImg.alt = Pics.all[numbers[2]].name;
+  // Pics.all[numbers[0]].amountShown += 1;
+  // Pics.all[numbers[1]].amountShown += 1;
+  // Pics.all[numbers[2]].amountShown += 1;
+  // console.log(numbers, 'currently showing');
+  // previouslyShown = numbers;
+}
+
+function showList() {
   var ulEl = document.getElementById('list')
-  for(var i = 0; i < Pics.all.length; i++);
+  for (var i = 0; i < Pics.all.length; i++);
 
   var liEl = document.createElement('li');
 
-    liEl.textContent = Pics.all[i].name + ' was shown ' +
+  liEl.textContent = Pics.all[i].name + ' was shown ' +
     Pictures.all[i].amountShown + ' times was clicked ' +
     Pics.all[i].name
-    ulEl.appendChild(liEl);
-  }
+  ulEl.appendChild(liEl);
+}
 
 
-function handleClick(e){
+function handleClick(e) {
   Pics.totalClicks += 1;
   console.log(Pics.totalClicks, 'total clicks');
-  for(var i = 0; i < Pics.all.length; i++);
-  if(e.target.alt === Pics.all[i].name){
+  for (var i = 0; i < Pics.all.length; i++);
+  if (e.target.alt === Pics.all[i].name) {
     //tally a click
     Pics.all[i].timesClicked += 1;
   }
 }
-  if(Pics.totalClicks === 25) {
-    //removed the event listener.
-    Pics.container.addEventListener('click', handleClick);
-    //displayed a list of products and show/clicks.
-    // return showList();
-  }
+if (Pics.totalClicks === 25) {
+  //removed the event listener.
+  Pics.container.addEventListener('click', handleClick);
+  //displayed a list of products and show/clicks.
+  showList();
+}
 
 
 displayImages();
