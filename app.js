89035a8c51620cb153c0c6928e3c6e56e1ead6e2
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-26 17:17:50 -0700

    ta help

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 'use strict';
 
+var completePics = [];
+var totalClicks = 0;
+var previouslyShown = [];
 Pics.all = [];
 
-Pics.allNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'wine-glass.jpg', 'water-can.jpg'];
+Pics.allNames = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg',
+  'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg',
+  'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png',
+  'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'wine-glass.jpg', 'water-can.jpg'
+];
 
 function Pics(name) {
   this.name = name;
   this.source = 'bus-images/' + this.name;
   this.amountShown = 0;
   this.timesClicked = 0;
-  Pics.all.push(this);
-}
-// debugger;
-for (var i = 0; i < Pics.allNames.length; i++) {
-  new Pics(Pics.allNames[i]);
+  // Pics.all.push(this);
 }
 
-Pics.leftImg = document.getElementById('left');
-Pics.centerImg = document.getElementById('center');
-Pics.rightImg = document.getElementById('right');
-Pics.container = document.getElementById('imageContainer');
+
+// debugger;
+for (var i = 0; i < Pics.allNames.length; i++) {
+  Pics.all.push(new Pics(Pics.allNames[i]));
+}
+
+var container = document.getElementById('imageContainer');
+var leftImg = document.getElementById('left');
+var centerImg = document.getElementById('center');
+var rightImg = document.getElementById('right');
 
 function randomNum() {
   return Math.floor(Math.random() * Pics.all.length);
@@ -34,57 +43,65 @@ function displayImages() {
 
   while (numbers[0] === numbers[1]) {
     console.log('Dupe prevented');
-  numbers[1] = randomNum();
-}
+    numbers[1] = randomNum();
+  }
   numbers[2] = randomNum();
   while (numbers[2] === numbers[1] || numbers[2] ===
     numbers[0]) {
     numbers[2] = randomNum();
   }
-  Pics.leftImg.src = Pics.all[randomNum()].source;
-  Pics.centerImg.src = Pics.all[randomNum()].source;
-  Pics.rightImg.src = Pics.all[randomNum()].source;
-  Pics.leftImg.src = Pics.all[numbers[0]].source;
-  Pics.centerImg.src = Pics.all[numbers[1]].source;
-  Pics.rightImg.src = Pics.all[numbers[2]].source;
-  Pics.leftImg.alt = Pics.all[numbers[0]].name;
-  Pics.centerImg.alt = Pics.all[numbers[1]].name;
-  Pics.rightImg.alt = Pics.all[numbers[2]].name;
-  Pics.all[numbers[0]].amountShown += 1;
-  Pics.all[numbers[1]].amountShown += 1;
-  Pics.all[numbers[2]].amountShown += 1;
+}
+
+function drawImgs() {
+
+  // leftImg.src = Pics.all[randomNum()].source;
+  // leftImg.src = Pics.all[numbers[0]].source;
+  // leftImg.imageContainer = Pics.all[numbers[0]].name;
+  // Pics.all[numbers[0]].amountShown += 1;
+
+  leftImg.setAttribute('src', Pics.all[previouslyShown[0]].source);
+  leftImg.dataset.productIndex = previouslyShown[0];
+  Pics.all[previouslyShown[0]].amountShown++;
+
+  centerImg.src = Pics.all[randomNum()].source;
+  centerImg.src = Pics.all[numbers[1]].source;
+  centerImg.imageContainer = Pics.all[numbers[1]].name;
+  all[numbers[1]].amountShown += 1;
+
+  rightImg.src = Pics.all[randomNum()].source;
+  rightImg.src = Pics.all[numbers[2]].source;
+  rightImg.imageContainer = Pics.all[numbers[2]].name;
+  all[numbers[2]].amountShown += 1;
+
   console.log(numbers, 'currently showing');
+}
+drawImgs();
+// function showList() {
+//   var containerEl = document.getElementById('imageContainer');
+//   for (var i = 0; i < Pics.all.length; i++);
+//
+//   var liEl = document.createElement('li');
+//
+//   liEl.textContent = Pics.all[i].name + ' was shown ' +
+//     Pics.all[i].amountShown + ' times was clicked ' +
+//     Pics.all[i].name;
+//   containerEl.appendChild(liEl);
+// }
+container.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  console.log('click', event);
+  console.log(event.target.dataset.productIndex, 'blahh');
+  completePics[event.target.dataset.productIndex].timesClicked++;
 
 }
-
-function showList() {
-  var ulEl = document.getElementById('list')
-  for (var i = 0; i < Pics.all.length; i++);
-
-  var liEl = document.createElement('li');
-
-  liEl.textContent = Pics.all[i].name + ' was shown ' +
-    Pictures.all[i].amountShown + ' times was clicked ' +
-    Pics.all[i].name
-  ulEl.appendChild(liEl);
-}
-
-
-function handleClick(e) {
-  Pics.totalClicks += 1;
-  console.log(Pics.totalClicks, 'total clicks');
-  for (var i = 0; i < Pics.all.length; i++);
-  if (e.target.alt === Pics.all[i].name) {
-    //tally a click
-    Pics.all[i].timesClicked += 1;
-  }
-}
-if (Pics.totalClicks === 25) {
-  //removed the event listener.
-  Pics.container.addEventListener('click', handleClick);
-  //displayed a list of products and show/clicks.
-  showList();
-}
-
+// Pics.totalClicks += 1;
+// for (var i = 0; i < Pics.all.length; i++) {
+//   if (e.target.imageContainer === Pics.all[i].name) {
+//     Pics.all[i].timesClicked += 1;
+// if (Pics.totalClicks === 5) {
+//   console.log(Pics.totalClicks, 'total clicks');
+//   //   Pics.container.removeEventListener('click', handleClick)
+// showList();
 
 displayImages();
